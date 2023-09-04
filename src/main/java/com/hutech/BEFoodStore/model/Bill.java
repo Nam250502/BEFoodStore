57621b2df7c77da457d37a417864182c51df1bb3
Nguyễn Văn Nam <namnguyen250502@gmail.com>
2023-09-04 14:27:39 +0700

    update class biil

--- src/main/java/com/hutech/BEFoodStore/model/Bill.java
+++ src/main/java/com/hutech/BEFoodStore/model/Bill.java
@@ -3,6 +3,7 @@ package com.hutech.BEFoodStore.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.sql.Date;
 import java.util.List;
 
 @Entity
@@ -11,6 +12,7 @@ public class Bill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
+    private Date date;
     private  double total;
 
     @OneToMany(mappedBy = "bill")
